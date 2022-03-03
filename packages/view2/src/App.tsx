91009aa81002bb27841afb3aa7a256763af87179
Cristiano Belloni <cristiano.belloni@jpmorgan.com>
2022-03-03 18:38:24 +0000

    Add price ticker

--- packages/view2/src/App.tsx
+++ packages/view2/src/App.tsx
@@ -1,16 +1,34 @@
 import 'regular-table/dist/css/material.css';
 import React, { useEffect, useRef } from 'react';
 import 'regular-table';
-import type { RegularTableElement, DataResponse } from 'regular-table';
-import { name, jobTitle, streetAddress, cityName, domainName } from 'minifaker';
+import type {
+  RegularTableElement,
+  DataResponse,
+  MetaData,
+} from 'regular-table';
+import { country, domainName, word } from 'minifaker';
 import 'minifaker/locales/en';
+
+interface ExtendedResponse extends DataResponse {
+  metadata: string[][];
+}
+
+interface ExtendedMetadata extends MetaData {
+  user: 'green' | 'red';
+}
+
+const colors = { green: '#e4ebe4', red: '#f1dbdb' };
 
 function createPeople(n: number) {
   const map = [
-    name,
-    jobTitle,
-    () => `${streetAddress()}, ${cityName()}`,
+    () =>
+      `${capitalize(word({ type: 'adjective' }))} ${capitalize(
+        word({ type: 'noun' }),
+      )}`,
+    () => String(Math.ceil(Math.random() * 100)),
+    () => country({ useCode: 'alpha3' }),
     domainName,
+    () => String((Math.random() * 1000).toFixed(2)),
   ];
 
   const columns: string[][] = [];
@@ -28,23 +46,32 @@ export default function App(): JSX.Element {
   const view = useRef<HTMLDivElement>(null);
   const regTableInstance = useRef<unknown>(null);
 
-  const people: string[][] = createPeople(7000);
+  const people: string[][] = createPeople(5000);
 
   useEffect(() => {
     const regularTable = document.createElement('regular-table');
     regTableInstance.current = regularTable;
     view.current?.appendChild(regularTable);
     function getDataSlice(x0: number, y0: number, x1: number, y1: number) {
-      const res: DataResponse = {
+      const metadata = [];
+      for (let i = 0; i < people[4].length; i += 1) {
+        const priceInc = Math.random() * 100 - 50;
+        people[4][i] = String((parseFloat(people[4][i]) + priceInc).toFixed(2));
+        metadata[i] = priceInc < 0 ? 'red' : 'green';
+      }
+
+      const res: ExtendedResponse = {
         num_rows: people[0].length,
         num_columns: people.length,
         data: people.slice(x0, x1).map((col) => col.slice(y0, y1)),
         column_headers: [
-          ['Name'],
-          ['Job Title'],
-          ['Address'],
-          ['Internet Domain'],
+          ['Company Name'],
+          ['Employees'],
+          ['Country'],
+          ['Domain'],
+          ['Price'],
         ],
+        metadata: [[], [], [], [], metadata],
       };
       return Promise.resolve(res);
     }
@@ -58,12 +85,28 @@ export default function App(): JSX.Element {
         th.style.fontWeight = '900';
         th.style.padding = '0 0 12px 0';
       }
+      const thd: NodeListOf<HTMLTableCellElement> =
+        table.querySelectorAll('body td');
+      for (const td of thd) {
+        const meta = table.getMeta(td) as ExtendedMetadata;
+        if (meta.user) {
+          td.style.backgroundColor = colors[meta.user];
+        }
+      }
     });
   });
 
   useEffect(() => {
     (regTableInstance.current as RegularTableElement).draw();
+    setInterval(
+      () => (regTableInstance.current as RegularTableElement).draw(),
+      1000,
+    );
   });
 
   return <div ref={view}></div>;
+}
+
+function capitalize(s: string) {
+  return s[0].toUpperCase() + s.slice(1);
 }
